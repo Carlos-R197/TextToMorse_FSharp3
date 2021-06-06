5dab7665ff95a4a97437b320163cec2b1d277a3a
Carlos Roque <carlosroque198@gmail.com>
2021-06-06 10:21:02 -0400

    Third test passed

--- Program.fs
+++ Program.fs
@@ -1,5 +1,30 @@
 module Library
 
+open System.Collections.Generic
+
+let dict : IDictionary<char, string> =
+                [ 
+                    'A',".-"; 'B',"-...";
+                    'C',"-.-."; 'D',"-.."; 'E',".";
+                    'F',"..-."; 'G',"--."; 'H',"....";
+                    'I',".."; 'J',".---"; 'K',"-.   -";
+                    'L',".-.."; 'M',"--"; 'N',"-.";
+                    'O',"---"; 'P',".--."; 'Q',"--.-";
+                    'R',".-."; 'S',"..."; 'T',"-";
+                    'U',"..-"; 'V',"...-"; 'W',".--";
+                    'X',"-..-"; 'Y',"-.--"; 'Z',"--..";
+                    '1',".----"; '2',"..---"; '3',"...--";
+                    '4',"....-"; '5',"....."; '6',"-....";
+                    '7',"--..."; '8',"---.."; '9',"----.";
+                    '0',"-----"; ',', "--..--"; '.',".-.-.-";
+                    '?',"..--.."; '/',"-..-."; '-',"-....-";
+                    '(',"-.--."; ')',"-.--.-"; ' ', "/"
+                ] |> dict
+
 module KataProblem =
     let text_to_morse text =
-        text
+        let mutable morse_text = ""
+        for character in text do
+            let morse_translation = dict.[System.Char.ToUpper character]
+            morse_text <- morse_text + morse_translation + " "
+        morse_text.TrimEnd()
--- Tests.fs
+++ Tests.fs
@@ -7,3 +7,11 @@ open Library.KataProblem
 [<Fact>]
 let empty_string_test () =
     Assert.Equal("", text_to_morse(""))
+
+[<Fact>]
+let one_letter_test () =
+    Assert.Equal("-", text_to_morse("T"))
+
+[<Fact>]
+let one_word_test () =
+    Assert.Equal("- --- -.. .- -.--", text_to_morse("Today"))
